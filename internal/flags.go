9121a7e1185527de73e06f598593e5151a9a2b48
maobaolong <307499405@qq.com>
2020-07-24 14:41:27 +0800

    Add a partSize parameter. (merge request !2)
    Squash merge branch 'partSize' into 'master'
    
    * Add a partSize parameter.

--- internal/flags.go
+++ internal/flags.go
@@ -138,6 +138,11 @@ func NewApp() (app *cli.App) {
 				Usage: "GID owner of all inodes.",
 			},
 
+			cli.Uint64Flag{
+				Name:  "MPUPartSize",
+				Value: 5 * 1024 * 1024,
+				Usage: "MPU part size.",
+			},
 			/////////////////////////
 			// S3
 			/////////////////////////
@@ -341,6 +346,9 @@ func PopulateFlags(c *cli.Context) (ret *FlagStorage) {
 		DebugFuse:  c.Bool("debug_fuse"),
 		DebugS3:    c.Bool("debug_s3"),
 		Foreground: c.Bool("f"),
+
+		// MPU
+		MPUPartSize: c.Uint64("MPUPartSize"),
 	}
 
 	// S3
